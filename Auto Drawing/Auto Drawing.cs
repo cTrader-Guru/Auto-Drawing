9a128b04d7216109352dae042f363f947978d7cd
Leonardo <leonardo.ciaccio@gmail.com>
2020-05-14 16:57:26 +0200

    Pulizia del codice in linea con il template 1.0.6

--- Auto Drawing/Auto Drawing.cs
+++ Auto Drawing/Auto Drawing.cs
@@ -1,4 +1,4 @@
-﻿/*  CTRADER GURU --> Template 1.0.3 --> https://ctrader.com/algos/indicators/show/2043
+﻿/*  CTRADER GURU --> Template 1.0.6 --> https://ctrader.com/algos/indicators/show/2043
 
     Homepage    : https://ctrader.guru/
     Telegram    : https://t.me/ctraderguru
@@ -12,21 +12,13 @@
 
 using System;
 using cAlgo.API;
-using System.Net;
-using System.Text;
 using System.Linq;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
-using System.Collections.Specialized;
-
-// --> Microsoft Visual Studio 2017 --> Strumenti --> Gestione pacchetti NuGet --> Gestisci pacchetti NuGet per la soluzione... --> Installa 
-using Newtonsoft.Json;
 
 namespace cAlgo
 {
 
-    // --> AccessRights = AccessRights.FullAccess se si vuole controllare gli aggiornamenti
-    [Indicator(IsOverlay = true, TimeZone = TimeZones.UTC, AccessRights = AccessRights.FullAccess)]
+    [Indicator(IsOverlay = true, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
     public class AutoDrawing : Indicator
     {
 
@@ -100,11 +92,6 @@ namespace cAlgo
         #region Identity
 
         /// <summary>
-        /// ID prodotto, identificativo, viene fornito da ctrader.guru, 74909 è il riferimento del template in uso
-        /// </summary>
-        public const int ID = 75035;
-
-        /// <summary>
         /// Nome del prodotto, identificativo, da modificare con il nome della propria creazione
         /// </summary>
         public const string NAME = "Auto Drawing";
@@ -112,7 +99,7 @@ namespace cAlgo
         /// <summary>
         /// La versione del prodotto, progressivo, utilie per controllare gli aggiornamenti se viene reso disponibile sul sito ctrader.guru
         /// </summary>
-        public const string VERSION = "1.0.1";
+        public const string VERSION = "1.0.2";
 
         #endregion
 
@@ -122,7 +109,7 @@ namespace cAlgo
         /// Identità del prodotto nel contesto di ctrader.guru
         /// </summary>
         [Parameter(NAME + " " + VERSION, Group = "Identity", DefaultValue = "https://ctrader.guru/product/auto-drawing/")]
-        public string ProductInfo { get; private set; }
+        public string ProductInfo { get; set; }
 
         /// <summary>
         /// Il colore degli strumenti disegnati sul grafico in formato stringa
@@ -197,7 +184,7 @@ namespace cAlgo
         /// <summary>
         /// I livelli standard di Fibonacci
         /// </summary>
-        readonly decimal[] DefaultFiboLevels = new[] 
+        readonly decimal[] DefaultFiboLevels = new[]
         {
             0.0m,
             23.6m,
@@ -225,9 +212,6 @@ namespace cAlgo
 
             // --> Stampo nei log la versione corrente
             Print("{0} : {1}", NAME, VERSION);
-
-            // --> Se viene settato l'ID effettua un controllo per verificare eventuali aggiornamenti
-            _checkProductUpdate();
 
             // --> L'utente potrebbe inserire un colore malforme
             if (Color.FromName(PBackgroundColorString).ToArgb() == 0)
@@ -271,57 +255,6 @@ namespace cAlgo
         #region Private Methods
 
         /// <summary>
-        /// Effettua un controllo sul sito ctrader.guru per mezzo delle API per verificare la presenza di aggiornamenti, solo in realtime
-        /// </summary>
-        private void _checkProductUpdate()
-        {
-
-            // --> Controllo solo se solo in realtime, evito le chiamate in backtest
-            if (RunningMode != RunningMode.RealTime)
-                return;
-
-            // --> Organizzo i dati per la richiesta degli aggiornamenti
-            Guru.API.RequestProductInfo Request = new Guru.API.RequestProductInfo 
-            {
-
-                MyProduct = new Guru.Product 
-                {
-
-                    ID = ID,
-                    Name = NAME,
-                    Version = VERSION
-
-                },
-                AccountBroker = Account.BrokerName,
-                AccountNumber = Account.Number
-
-            };
-
-            // --> Effettuo la richiesta
-            Guru.API Response = new Guru.API(Request);
-
-            // --> Controllo per prima cosa la presenza di errori di comunicazioni
-            if (Response.ProductInfo.Exception != "")
-            {
-
-                Print("{0} Exception : {1}", NAME, Response.ProductInfo.Exception);
-
-            }
-            // --> Chiedo conferma della presenza di nuovi aggiornamenti
-            else if (Response.HaveNewUpdate())
-            {
-
-                string updatemex = string.Format("{0} : Updates available {1} ( {2} )", NAME, Response.ProductInfo.LastProduct.Version, Response.ProductInfo.LastProduct.Updated);
-
-                // --> Informo l'utente con un messaggio sul grafico e nei log del cbot
-                Chart.DrawStaticText(NAME + "Updates", updatemex, VerticalAlignment.Top, HorizontalAlignment.Left, Color.Red);
-                Print(updatemex);
-
-            }
-
-        }
-
-        /// <summary>
         /// Formalizza i tasti per il controllo popup
         /// </summary>
         private void _createDrawingDialog()
@@ -329,7 +262,7 @@ namespace cAlgo
 
             string separator = "---------------------";
 
-            var stackPanel = new StackPanel 
+            var stackPanel = new StackPanel
             {
                 VerticalAlignment = VerticalAlignment.Top,
                 HorizontalAlignment = HorizontalAlignment.Left,
@@ -339,75 +272,75 @@ namespace cAlgo
                 BackgroundColor = PBackgroundColor
             };
 
-            var equiChannelButton = new Button 
+            var equiChannelButton = new Button
             {
                 Text = "Equidistant Channel",
                 HorizontalContentAlignment = HorizontalAlignment.Left
             };
 
-            var linearRegressionButton = new Button 
+            var linearRegressionButton = new Button
             {
                 Text = "Linear Regression",
                 HorizontalContentAlignment = HorizontalAlignment.Left
             };
 
-            var fibonacciRetracementButton = new Button 
+            var fibonacciRetracementButton = new Button
             {
                 Text = "Fibonacci Retracement",
                 HorizontalContentAlignment = HorizontalAlignment.Left
             };
 
-            var supportTrendLineButton = new Button 
+            var supportTrendLineButton = new Button
             {
                 Text = "Support Trend",
                 HorizontalContentAlignment = HorizontalAlignment.Left
             };
 
-            var resistanceTrendLineButton = new Button 
+            var resistanceTrendLineButton = new Button
             {
                 Text = "Resistance Trend",
                 HorizontalContentAlignment = HorizontalAlignment.Left
             };
 
-            var resistanceSupportTrendLineButton = new Button 
+            var resistanceSupportTrendLineButton = new Button
             {
                 Text = "Rex / Sup Trend",
                 HorizontalContentAlignment = HorizontalAlignment.Left
             };
 
-            var supportLevelButton = new Button 
+            var supportLevelButton = new Button
             {
                 Text = "Support",
                 HorizontalContentAlignment = HorizontalAlignment.Left
             };
 
-            var resistanceLevelButton = new Button 
+            var resistanceLevelButton = new Button
             {
                 Text = "Resistance",
                 HorizontalContentAlignment = HorizontalAlignment.Left
             };
 
-            var removeAllObjButton = new Button 
+            var removeAllObjButton = new Button
             {
                 Text = "Remove all (Shift + Click)",
                 HorizontalContentAlignment = HorizontalAlignment.Left
             };
 
-            var resistanceSupportLevelButton = new Button 
+            var resistanceSupportLevelButton = new Button
             {
                 Text = "Rex / Sup Level",
                 HorizontalContentAlignment = HorizontalAlignment.Left
 
             };
 
-            var space = new Button 
+            var space = new Button
             {
                 Text = separator,
                 HorizontalContentAlignment = HorizontalAlignment.Center
 
             };
 
-            var space2 = new Button 
+            var space2 = new Button
             {
                 Text = separator,
                 HorizontalContentAlignment = HorizontalAlignment.Center
@@ -1009,185 +942,6 @@ namespace cAlgo
         }
 
         #endregion
-
-    }
-
-}
-
-/// <summary>
-/// NameSpace che racchiude tutte le feature ctrader.guru
-/// </summary>
-namespace Guru
-{
-    /// <summary>
-    /// Classe che definisce lo standard identificativo del prodotto nel marketplace ctrader.guru
-    /// </summary>
-    public class Product
-    {
-
-        public int ID = 0;
-        public string Name = "";
-        public string Version = "";
-        public string Updated = "";
-
-    }
-
-    /// <summary>
-    /// Offre la possibilità di utilizzare le API messe a disposizione da ctrader.guru per verificare gli aggiornamenti del prodotto.
-    /// Permessi utente "AccessRights = AccessRights.FullAccess" per accedere a internet ed utilizzare JSON
-    /// </summary>
-    public class API
-    {
-        /// <summary>
-        /// Costante da non modificare, corrisponde alla pagina dei servizi API
-        /// </summary>
-        private const string Service = "https://ctrader.guru/api/product_info/";
-
-        /// <summary>
-        /// Costante da non modificare, utilizzata per filtrare le richieste
-        /// </summary>
-        private const string UserAgent = "cTrader Guru";
-
-        /// <summary>
-        /// Variabile dove verranno inserite le direttive per la richiesta
-        /// </summary>
-        private RequestProductInfo RequestProduct = new RequestProductInfo();
-
-        /// <summary>
-        /// Variabile dove verranno inserite le informazioni identificative dal server dopo l'inizializzazione della classe API
-        /// </summary>
-        public ResponseProductInfo ProductInfo = new ResponseProductInfo();
-
-        /// <summary>
-        /// Classe che formalizza i parametri di richiesta, vengono inviate le informazioni del prodotto e di profilazione a fini statistici
-        /// </summary>
-        public class RequestProductInfo
-        {
-
-            /// <summary>
-            /// Il prodotto corrente per il quale richiediamo le informazioni
-            /// </summary>
-            public Product MyProduct = new Product();
-
-            /// <summary>
-            /// Broker con il quale effettiamo la richiesta
-            /// </summary>
-            public string AccountBroker = "";
-
-            /// <summary>
-            /// Il numero di conto con il quale chiediamo le informazioni
-            /// </summary>
-            public int AccountNumber = 0;
-
-        }
-
-        /// <summary>
-        /// Classe che formalizza lo standard per identificare le informazioni del prodotto
-        /// </summary>
-        public class ResponseProductInfo
-        {
-
-            /// <summary>
-            /// Il prodotto corrente per il quale vengono fornite le informazioni
-            /// </summary>
-            public Product LastProduct = new Product();
-
-            /// <summary>
-            /// Eccezioni in fase di richiesta al server, da utilizzare per controllare l'esito della comunicazione
-            /// </summary>
-            public string Exception = "";
-
-            /// <summary>
-            /// La risposta del server
-            /// </summary>
-            public string Source = "";
-
-        }
-
-        /// <summary>
-        /// Richiede le informazioni del prodotto richiesto
-        /// </summary>
-        /// <param name="Request"></param>
-        public API(RequestProductInfo Request)
-        {
-
-            RequestProduct = Request;
-
-            // --> Non controllo se non ho l'ID del prodotto
-            if (Request.MyProduct.ID <= 0)
-                return;
-
-            // --> Dobbiamo supervisionare la chiamata per registrare l'eccexione
-            try
-            {
-
-                // --> Strutturo le informazioni per la richiesta POST
-                NameValueCollection data = new NameValueCollection 
-                {
-                    {
-                        "account_broker",
-                        Request.AccountBroker
-                    },
-                    {
-                        "account_number",
-                        Request.AccountNumber.ToString()
-                    },
-                    {
-                        "my_version",
-                        Request.MyProduct.Version
-                    },
-                    {
-                        "productid",
-                        Request.MyProduct.ID.ToString()
-                    }
-                };
-
-                // --> Autorizzo tutte le pagine di questo dominio
-                Uri myuri = new Uri(Service);
-                string pattern = string.Format("{0}://{1}/.*", myuri.Scheme, myuri.Host);
-
-                Regex urlRegEx = new Regex(pattern);
-                WebPermission p = new WebPermission(NetworkAccess.Connect, urlRegEx);
-                p.Assert();
-
-                // --> Protocollo di sicurezza https://
-                ServicePointManager.SecurityProtocol = (SecurityProtocolType)192 | (SecurityProtocolType)768 | (SecurityProtocolType)3072;
-
-                // -->> Richiedo le informazioni al server
-                using (var wb = new WebClient())
-                {
-
-                    wb.Headers.Add("User-Agent", UserAgent);
-
-                    var response = wb.UploadValues(myuri, "POST", data);
-                    ProductInfo.Source = Encoding.UTF8.GetString(response);
-
-                }
-
-                // -->>> Nel cBot necessita l'attivazione di "AccessRights = AccessRights.FullAccess"
-                ProductInfo.LastProduct = JsonConvert.DeserializeObject<Product>(ProductInfo.Source);
-
-            } catch (Exception Exp)
-            {
-
-                // --> Qualcosa è andato storto, registro l'eccezione
-                ProductInfo.Exception = Exp.Message;
-
-            }
-
-        }
-
-        /// <summary>
-        /// Esegue un confronto tra le versioni per determinare la presenza di aggiornamenti
-        /// </summary>
-        /// <returns></returns>
-        public bool HaveNewUpdate()
-        {
-
-            // --> Voglio essere sicuro che stiamo lavorando con le informazioni giuste
-            return (ProductInfo.LastProduct.ID == RequestProduct.MyProduct.ID && ProductInfo.LastProduct.Version != "" && RequestProduct.MyProduct.Version != "" && new Version(RequestProduct.MyProduct.Version).CompareTo(new Version(ProductInfo.LastProduct.Version)) < 0);
-
-        }
 
     }
 
